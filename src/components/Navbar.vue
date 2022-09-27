2c0daa16977fcca58f8c4b08b855ef6fdd740adb
jason <pnxl@pm.me>
2022-09-27 20:03:40 +0700

    add navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex w-full justify-center">
+    <div class="w-[48rem]">
+      <div class="flex w-full justify-between">
+        <router-link
+          to="/"
+          :class="
+            $route.path === '/'
+              ? `cursor-default font-semibold text-lime-600 dark:text-lime-400`
+              : `underline transition hover:text-lime-700 dark:hover:text-lime-200`
+          "
+          >Home</router-link
+        >
+
+        <div class="flex">
+          <router-link
+            to="/employees"
+            :class="
+              $route.path === '/employees'
+                ? `cursor-default font-semibold text-lime-600 dark:text-lime-400`
+                : `underline transition hover:text-lime-700 dark:hover:text-lime-200`
+            "
+            >Employees</router-link
+          >
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <main class="py-8">
+    <Navbar />
+  </main>
+</template>
+
+<script setup lang="ts">
+import Navbar from "@/components/Navbar.vue";
+</script>
