ccb394f116a34cbe1eaac235dcf1bcd1b4a9e66b
jason <pnxl@pm.me>
2022-10-15 12:58:34 +0700

    add footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex w-full justify-center">
+    <div class="w-[48rem]">
+      <div class="flex w-full justify-between px-8 md:px-0">
+        <div class="text-neutral-500 dark:text-neutral-400">
+          Copyright © 2022 Fang Industries, inc.
+        </div>
+        <div class="flex">
+          <router-link
+            to="/licenses"
+            :class="
+              $route.path === '/licenses'
+                ? `cursor-default font-semibold text-lime-600 dark:text-lime-400`
+                : `underline transition hover:text-lime-700 dark:hover:text-lime-200`
+            "
+            >Licenses</router-link
+          >
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
