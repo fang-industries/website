1d094fb15a56f234a470f74b2f275db2a884240f
jason <pnxl@pm.me>
2022-10-17 20:01:13 +0700

    match branding page with design guidelines

--- src/pages/branding.vue
+++ src/pages/branding.vue
@@ -1,41 +1,41 @@
 <template>
   <main class="flex flex-col gap-y-16 py-8">
     <Navbar />
-    <div class="min-w-screen flex justify-center px-8 md:px-0">
-      <div class="flex w-[48rem] flex-col gap-y-16 text-center">
-        <div class="flex flex-col gap-y-8">
-          <div class="flex flex-col gap-y-4">
-            <h1 class="text-5xl font-semibold">Design & Branding</h1>
-            <p>
+    <main class="min-w-screen flex justify-center px-8 md:px-0">
+      <div class="flex w-[48rem] flex-col gap-y-16">
+        <div class="flex flex-col gap-y-16">
+          <section id="heading" class="flex flex-col gap-y-4">
+            <h1 class="text-4xl font-semibold">Design & Branding</h1>
+            <p class="text-neutral-500 dark:text-neutral-400">
               The Fang Industries, inc. brand belongs to Pix3l_, the entity that
               guides and supports Fang Industries, inc. These guidelines
               describe when and how the brand should be used.
             </p>
-          </div>
-          <div class="flex flex-col gap-y-4">
-            <h2 class="text-4xl font-medium">Brand Marks</h2>
-            <p>
+          </section>
+          <section id="brand" class="flex flex-col gap-y-4">
+            <h2 class="text-3xl font-semibold">Brand Marks</h2>
+            <p class="text-neutral-600 dark:text-neutral-300">
               Fang Industries, inc. claims ownership over one mark: The
               <strong>Fang Industries, inc. logo-type</strong>.
             </p>
-          </div>
-          <div class="flex flex-col gap-y-4">
-            <h2 class="text-xl font-medium">
-              Both said marks should be used with the following in mind:
-            </h2>
-            <ul>
-              <li>
-                Do not stretch, skew, rotate, flip, or otherwise alter the
-                marks.
-              </li>
-              <li>
-                Do not use the marks on an overly-busy background; solid colours
-                or gradients work best.
-              </li>
-            </ul>
-          </div>
+            <div class="flex flex-col gap-y-4">
+              <h2 class="text-xl font-medium">
+                Both said marks should be used with the following in mind:
+              </h2>
+              <ul class="ml-6 list-disc text-neutral-600 dark:text-neutral-300">
+                <li>
+                  Do not stretch, skew, rotate, flip, or otherwise alter the
+                  marks.
+                </li>
+                <li>
+                  Do not use the marks on an overly-busy background; solid
+                  colours or gradients work best.
+                </li>
+              </ul>
+            </div>
+          </section>
         </div>
-        <div class="flex flex-col gap-y-8">
+        <section id="logos" class="flex flex-col gap-y-8 text-center">
           <div class="flex flex-col gap-y-4">
             <img
               src="@/assets/branding/fard.svg"
@@ -74,9 +74,9 @@
               </figcaption>
             </div>
           </div>
-        </div>
+        </section>
       </div>
-    </div>
+    </main>
     <Footer />
   </main>
 </template>
